32c3c9baaa358d50a1bfa5c1a0ba5934a2a331bb
Georgia Kalyva <georgia_k22@hotmail.com>
2020-05-04 13:44:21 +0300

    Initial Commit

---
+++ ExcelComparer/ListData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelComparer
+{
+    public class ListData
+    {
+        public string Subsite { get; set; }
+        public string ListName { get; set; }
+        public int RecordCount { get; set; }
+        public int RecordCountDestination { get; set; }
+        public string Hidden { get; set; }
+
+    }
+
+}
